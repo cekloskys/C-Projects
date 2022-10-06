7800efbf2803956a7a4a49d31fa860a40e7d077e
Sue Ceklosky <cekloskys@chc.edu>
2022-10-06 11:10:08 -0400

    October 6, 2022

---
+++ headers/BasicIO.h
@@ -0,0 +1,10 @@
+// include pre-processor directives
+#define FIRST_NAME "Susan"
+#define LAST_NAME "Ceklosky"
+#define WEIGHT 118.75
+#define AGE 53
+#include <stdio.h>
+
+// include function declarations
+void output(void);
+void input(void);
