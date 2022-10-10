9ab2fea550cfc64444246bf4fbd9a8bf3a795c82
Sue Ceklosky <cekloskys@chc.edu>
2022-10-10 11:45:38 -0400

    functions

--- App.c
+++ App.c
@@ -4,6 +4,7 @@
 #include "./headers/BasicIO.h"
 #include "./headers/Decisions.h"
 #include "./headers/Loops.h"
+#include "./headers/Functions.h"
 
 int main(void) {
     // puts("Hello World!!!");
@@ -13,9 +14,20 @@ int main(void) {
     // testing Decisions functionality
     // decisions1();
     // testing Loops functionality
-    loops1();
-    loops2();
-    loops3();
-    loops4();
-    loops5();
+    // loops1();
+    // loops2();
+    // loops3();
+    // loops4();
+    // loops5();
+    // testing Functions functionality
+    // declare three character arrays and initialize them
+    char student[] = "Sue";
+    char course[] = "Ruby";
+    char programmer[] = "Yukihiro Matsumoto";
+    congratulate1(student, course, programmer);
+    congratulate2(student, course, programmer);
+    printf("%.2f degrees celsius = %.2f degrees fahrenheit\n", 
+        0.0, fahrenheitFromCelsius(0.0));
+    printf("%.2f degrees celsius = %.2f degrees fahrenheit\n", 
+        100.0, fahrenheitFromCelsius(100.0));
 }
